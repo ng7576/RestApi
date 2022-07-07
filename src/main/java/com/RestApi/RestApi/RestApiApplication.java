49187e375a254738a73f0fbb7b1738f7e1bb792c
NISHAN GAUTAM <94755454+ng7576@users.noreply.github.com>
2022-07-07 19:44:21 -0400

    Update RestApiApplication.java

--- src/main/java/com/RestApi/RestApi/RestApiApplication.java
+++ src/main/java/com/RestApi/RestApi/RestApiApplication.java
@@ -3,6 +3,10 @@ package com.RestApi.RestApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+//IF YOU ARE NEW TO SPRING BOOT
+//PLEASE LOOK UP THESE --> BEANS, MAVEN. UNDERSTANDING WHAT THESE MEANT AND FUNCTION HELPED ME QUITE A BIT.
 @SpringBootApplication
 public class RestApiApplication {
 

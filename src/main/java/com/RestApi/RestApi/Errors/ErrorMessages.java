4c472a8a26626fa609a5400024d7d1fbc63a2da6
NISHAN GAUTAM <94755454+ng7576@users.noreply.github.com>
2022-07-07 19:42:33 -0400

    Update ErrorMessages.java

--- src/main/java/com/RestApi/RestApi/Errors/ErrorMessages.java
+++ src/main/java/com/RestApi/RestApi/Errors/ErrorMessages.java
@@ -5,7 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-
+//LOMBOK IS USED TO REDUCE BOILERPLATE CODE 
+// INSTEAD OF MANUALLY TYPING OUT GETTERS, SETTERS AND CONSTRUCTORS FOR THE CLASS, THIS CODE LETS LOMBOK HANDLE IT.
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
